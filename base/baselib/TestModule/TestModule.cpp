2cca36fd80f329f4633454d815b65cee03849065
chrosyou <646680299@qq.com>
2016-03-07 19:53:21 +0800

    module

--- base/baselib/TestModule/TestModule.cpp
+++ base/baselib/TestModule/TestModule.cpp
@@ -3,6 +3,11 @@
 
 #include "stdafx.h"
 #include "TestModule.h"
+#include "shlwapi.h"
+
+#pragma comment(lib, "shlwapi.lib")
+
+typedef int (__cdecl* lpFunc)();
 
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
@@ -13,7 +18,21 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
- 	
+	TCHAR szPath[MAX_PATH] = {0};
+	GetModuleFileName(NULL, szPath, MAX_PATH);
+	PathRemoveFileSpec(szPath);
+	PathAppend(szPath, _T("TestDll.dll"));
+
+
+ 	HMODULE hDll = LoadLibrary(szPath);
+	if (NULL == hDll) {
+		return ;
+	}
+
+	lpFunc pfunc=(lpFunc)::GetProcAddress(hDll, "run");
+
+
+
 	return 0;
 }
 
