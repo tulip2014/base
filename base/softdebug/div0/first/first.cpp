0e996c3176884c52154a97fc754dfa7e82e3b34d
chrosyou <646680299@qq.com>
2016-04-03 18:50:17 +0800

    windbg test

---
+++ base/softdebug/div0/first/first.cpp
@@ -0,0 +1,14 @@
+// first.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a = 1234;
+	std::cout<<a<<std::endl;
+	system("pause");
+	return 0;
+}
+
---
+++ base/softdebug/div0/second/second.cpp
@@ -0,0 +1,19 @@
+// second.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "second.h"
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPTSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: 在此放置代码。
+	
+	return 0;
+}
+
